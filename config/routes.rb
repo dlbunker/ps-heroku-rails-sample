21a9ecea31d251b39d0857633a09483723071fe4
Dan Bunker <dan.bunker@stgconsulting.com>
2017-08-14 22:52:31 -0600

    fixed endpoint prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :rivers, only: [:index]
+  get 'api/v1/rivers', to: "rivers#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
