53f261a89f618cf11ecc47df98726f1190979185
Dan Bunker <dan.bunker@stgconsulting.com>
2017-10-15 22:27:34 -0600

    puts change

--- app/controllers/rivers_controller.rb
+++ app/controllers/rivers_controller.rb
@@ -1,5 +1,6 @@
 class RiversController < ApplicationController
   def index
+    puts "I am in the river controller"
     render json: RestClient.get("http://riverbrain.com/api/v1/rivers", {})
   end
 end
